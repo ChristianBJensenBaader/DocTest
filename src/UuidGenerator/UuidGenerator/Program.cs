8214ffc2ac3d65cd87117fdd380c2ed69eb7d0b9
Christian Bøeg-Jensen <christian.bjensen@baader.com>
2024-06-18 13:48:53 +0200

    initial version

---
+++ src/UuidGenerator/UuidGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace UuidGenerator
+{
+  internal class Program
+  {
+    private static  async IAsyncEnumerable<Guid> GetSequentialGuidAsync(int max)
+    {
+      for (int i = 0; i < max; i++)
+      {
+        yield return UuidGenerator.NewUuid7();
+
+        await Task.Delay(100);
+      }
+    }
+    
+    static async Task Main(string[] args)
+    {
+      //var uuid7 = UuidGenerator.NewUuid7();
+      //var uuid4 = UuidGenerator.NewUuid4();
+
+      List<KeyValuePair<DateTime, Guid>> guidMap = new();
+
+      await foreach (var uuid7 in GetSequentialGuidAsync(10))
+      {
+        guidMap.Add(new KeyValuePair<DateTime, Guid>(DateTime.UtcNow, uuid7));
+      }
+
+      Console.WriteLine("As generated");
+      Console.WriteLine("-----------------------------------");
+      foreach (var (when, guid) in guidMap.OrderBy(x => x.Key))
+      {
+        Console.WriteLine($"Time: {when.Ticks}, guid: {guid}");
+      }
+      Console.WriteLine();
+      Console.WriteLine("As sorted");
+      Console.WriteLine("-----------------------------------");
+      foreach (var (when, guid) in guidMap.OrderBy(x => x.Value))
+      {
+        Console.WriteLine($"Time: {when.Ticks}, guid: {guid}");
+      }
+      
+      //Console.WriteLine($"UUID version 7: {uuid7}");
+      //Console.WriteLine($"UUID version 4: {uuid4}");
+    }
+  }
+}
---
+++ src/UuidGenerator/UuidGenerator/UuidGenerator.cs
@@ -0,0 +1,64 @@
+﻿// BAADER Code Cleanup
+using System.Security.Cryptography;
+
+namespace UuidGenerator;
+
+/// <summary>
+/// A utility class for generating UUIDs.
+/// </summary>
+public static class UuidGenerator
+{
+  /// <summary>
+  /// Generates a new UUIDv7 based on the current UTC time.
+  /// </summary>
+  /// <returns>A new UUIDv7 <see cref="Guid"/>.</returns>
+  public static Guid NewUuid7() => NewUuid7(DateTimeOffset.UtcNow);
+  
+  /// <summary>
+  /// Generates a new UUIDv7 based on the specified date and time.
+  /// </summary>
+  /// <param name="dateTimeOffset">The date and time to base the UUIDv7 on.</param>
+  /// <returns>A new UUIDv7 <see cref="Guid"/>.</returns>
+  public static Guid NewUuid7(DateTimeOffset dateTimeOffset)
+  {
+    Span<byte> uuidAsBytes = stackalloc byte[16];
+    FillTimePart(uuidAsBytes, dateTimeOffset);
+    var random_part = uuidAsBytes[6..];
+    RandomNumberGenerator.Fill(random_part);
+
+    //Set Version 0x7
+    uuidAsBytes[6] &= 0x0F;
+    uuidAsBytes[6] |= 0x70;
+
+    //Set variant 0x8 (RFC 9562)
+    uuidAsBytes[8] &= 0x3F;
+    uuidAsBytes[8] |= 0x80;
+    
+    return new Guid(uuidAsBytes, true);
+  }
+  
+  /// <summary>
+  /// Fills the first part of the UUID with the timestamp based on the specified date and time.
+  /// </summary>
+  /// <param name="uuidAsBytes">The byte span representing the UUID.</param>
+  /// <param name="dateTimeOffset">The date and time to base the UUIDv7 on.</param>
+  private static void FillTimePart(Span<byte> uuidAsBytes, DateTimeOffset dateTimeOffset)
+  {
+    long currentTimestamp = dateTimeOffset.ToUnixTimeMilliseconds();
+
+    Span<byte> current = stackalloc byte[8];
+    BitConverter.TryWriteBytes(current, currentTimestamp);
+    //var current = BitConverter.GetBytes(currentTimestamp).AsSpan();
+    if (BitConverter.IsLittleEndian)
+    {
+      current.Reverse();
+    }
+    current[2..8].CopyTo(uuidAsBytes);
+  }
+
+  /// <summary>
+  /// Generates a new UUIDv4.
+  /// </summary>
+  /// <returns>A new UUIDv4 <see cref="Guid"/>.</returns>
+  public static Guid NewUuid4() => Guid.NewGuid();
+}
